2ec40d380795639976be3e36866d2ed198b98547
Samuel Bortolin <samuel.bortolin@afliant.com>
2025-09-01 10:11:24 +0200

    Added remaining files

---
+++ ProofTracker copia/ProofTrackerTests/ProofTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProofTrackerTests.swift
+//  ProofTrackerTests
+//
+//  Created by Samuel Bortolin on 29/08/25.
+//
+
+import Testing
+@testable import ProofTracker
+
+struct ProofTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
